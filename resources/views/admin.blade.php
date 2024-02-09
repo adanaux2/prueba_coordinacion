bcf20fb916f9bc466134a936947082628245c353
ed-ma67 <158239064+ed-ma67@users.noreply.github.com>
2024-02-09 16:56:07 -0500

    optimizacion y logos

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,9 +5,16 @@
 @section('content')
     @if (auth()->user()->id_rol == 1)
         <p>Estás viendo esto porque eres un administrador.</p>
-        <h1>
-            admin
-        </h1>
+        <div class="small-box bg-warning">
+            <div class="inner">
+                 <h3>44</h3>
+                 <p>usuarios registrados</p>
+            </div>
+            <div class="icon">
+                <i class="fa-solid fa-address-card"></i>
+            </div>
+            <a href="" class="small-box-footer">Ver más <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
     @else
         <h1>No tienes accesos para este panel</h1>
     @endif
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,32 +2,15 @@
 <html lang="en">
 
 <head>
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('name') | Dashboard</title>
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
-    <!-- Theme style -->
     <link rel="stylesheet" href="css/adminlte.min.css">
-    <!-- overlayScrollbars -->
     <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
-    <!-- summernote -->
-    <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -35,8 +18,7 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60"
-                width="60">
+            <img class="animation__shake" src="img/logo-aztlan-1.png" alt="AdminLTELogo" height="160" width="140">
         </div>
 
         <!-- Navbar -->
@@ -90,24 +72,27 @@
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <aside class="main-sidebar sidebar-dark-primary ">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-                <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            {{-- <a href="index3.html" class="brand-link">
+                <img src="img/logo-aztlan.png" width="80" class="brand-image"
                     style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
-            </a>
+            
+            </a> --}}
+            {{-- <div>
+                <center><img src="img/logo-aztlan.png" width="150" ></center>
+            </div> --}}
 
             <!-- Sidebar -->
             <div class="sidebar">
                 <!-- Sidebar user panel (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div>
                     <div class="image">
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="img/logo-aztlan.png" width="190">
                     </div>
-                    <div class="info">
+                    {{-- <div class="info">
                         <a href="#" class="d-block">Bienbenido {{ auth()->user()->name }}</a>
-                    </div>
+                    </div> --}}
                 </div>
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
@@ -185,7 +170,7 @@
     <!-- ./wrapper -->
 
     <!-- jQuery -->
-    <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
     <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
@@ -199,8 +184,6 @@
     <!-- Sparkline -->
     <script src="plugins/sparklines/sparkline.js"></script>
     <!-- JQVMap -->
-    <script src="plugins/jqvmap/jquery.vmap.min.js"></script>
-    <script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
     <!-- jQuery Knob Chart -->
     <script src="plugins/jquery-knob/jquery.knob.min.js"></script>
     <!-- daterangepicker -->
