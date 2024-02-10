7e8c4e8fceeeaac3c54bbc63c0311c55af3a2298
ed-ma67 <158239064+ed-ma67@users.noreply.github.com>
2024-02-10 14:46:55 -0500

    apiMaestros

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,9 +5,35 @@
 @section('content')
     @if (auth()->user()->id_rol == 1)
         <p>Estás viendo esto porque eres un administrador.</p>
-        <h1>
-            admin
-        </h1>
+        {{-- Caja de usuarios registrados --}}
+        <div class="row">
+            <div class="col col-md-6">
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>44</h3>
+                        <p>usuarios registrados</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa-solid fa-address-card"></i>
+                    </div>
+                    <a href="" class="small-box-footer">Ver más <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            {{-- Caja para registrar usuarios --}}
+            <div class="col md-3">
+                <div class="small-box bg-warning">
+                    <div class="inner" style="background:rgb(29, 194, 70)">
+                        <h3>+</h3>
+                        <p>Registar Usuarios</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa-solid fa-address-book"></i>
+                    </div>
+                    <a href="" class="small-box-footer" style="background:rgb(25, 165, 60)">Ver más <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
     @else
         <h1>No tienes accesos para este panel</h1>
     @endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,14 +8,11 @@
 <body>
     @if(auth()->check())
     <div>
-        <h2>Bienbenido {{ auth()->user()->name}}</h2>
+        <h2>Bienvenido {{ auth()->user()->name}}</h2>
         <a href="{{ route('login.destroy') }}">Logout</a>
     </div>
     @else
-    <div>
-        <a href="{{route('login.index')}}">Log In</a>
-        <a href="{{route('register.index')}}">Register</a>
-    </div>
+
     @endif
 
 
